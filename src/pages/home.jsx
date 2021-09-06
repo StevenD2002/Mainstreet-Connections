951d6b4fc812179a5610f6c4b43d2abeea15e0c6
Steven Dew <stevendew@Stevens-MacBook-Pro-2.local>
2021-09-06 01:21:16 -0500

    🐛  FIX: fixes text issues

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -105,7 +105,7 @@ export function HomePage() {
             <img src={dollar} alt="money" className="about-icon" />
             <h3>Value</h3>
             <p>
-              As a veteran owned busines, we know the value of hard work. You
+              As a veteran owned business, we know the value of hard work. You
               can be sure that you are getting much more than your money's worth
               when you partner with us.
             </p>
@@ -123,12 +123,12 @@ export function HomePage() {
           <div className="group">
             <p>519 E Chester St,</p>
             <p>Jackson, TN 38301</p>
-            <p>1-888-824-2339</p>
           </div>
           <h2>Hours</h2>
           <div className="line"></div>
           <div className="group">
-            <p>8:00 - 5:00 Mon - Fri</p>
+            <p>Mon - Fri</p>
+            <p>8:00AM - 5:00PM </p>
           </div>
         </div>
         <div className="location-content">
